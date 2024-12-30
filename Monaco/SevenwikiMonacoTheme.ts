2d86fc75458a90b051c22ae8cb1507cd7cfa4f1a
shiüo <shiueo.csh@gmail.com>
2024-12-30 14:15:04 +0900

    Update SevenwikiMonacoTheme.ts

--- Monaco/SevenwikiMonacoTheme.ts
+++ Monaco/SevenwikiMonacoTheme.ts
@@ -1,22 +1,21 @@
 export const SevenwikiMonacoTheme = {
-    base: 'vs-dark',
-    inherit: true,
-    rules: [
-      { token: 'heading', foreground: '#FF0000' },
-      { token: 'bold', foreground: '#00FF00' },
-      { token: 'italic', foreground: '#0000FF' },
-      { token: 'link', foreground: '#FF00FF' },
-      { token: 'code', foreground: '#FFFF00' },
-      { token: 'text', foreground: '#FFFFFF' },
-    ],
-    colors: {
-      'editor.foreground': '#FFFFFF',
-      'editor.background': '#1E1E1E',
-      'editorCursor.foreground': '#FFFFFF',
-      'editorLineNumber.foreground': '#A0A0A0',
-      'editor.selectionBackground': '#4B4B4B',
-      'editor.lineHighlightBackground': '#2C2C2C',
-      'editorWhitespace.foreground': '#404040',
-    },
-  }
-  
+  base: 'vs-dark',
+  inherit: true,
+  rules: [
+    { token: 'heading', foreground: '#FF0000' },
+    { token: 'bold', foreground: '#FF7D7D', fontStyle: "bold" },
+    { token: 'italic', foreground: '#FFAE7D', fontStyle: "italic" },
+    { token: 'link', foreground: '#FF00FF' },
+    { token: 'code', foreground: '#FFFF00' },
+    { token: 'text', foreground: '#FFFFFF' },
+  ],
+  colors: {
+    'editor.foreground': '#FFFFFF',
+    'editor.background': '#1A1A1A',
+    'editorCursor.foreground': '#FFFFFF',
+    'editorLineNumber.foreground': '#A0A0A0',
+    'editor.selectionBackground': '#4B4B4B',
+    'editor.lineHighlightBackground': '#2C2C2C',
+    'editorWhitespace.foreground': '#404040',
+  },
+}
